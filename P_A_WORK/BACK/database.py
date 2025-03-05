9247c6c73121d6b93d19b5bc6ab35a981cdefa4f
denis181079@gmail.com <denis181079@gmail.com>
2025-03-05 12:25:59 +0700

    after black

--- P_A_WORK/BACK/database.py
+++ P_A_WORK/BACK/database.py
@@ -1,9 +1,9 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-#DATABASE_URL = "sqlite+aiosqlite:///./DB/tw.db"
+# DATABASE_URL = "sqlite+aiosqlite:///./DB/tw.db"
 
-DATABASE_URL ='postgresql+asyncpg://admin:admin@postgres_container:5432/admin'
+DATABASE_URL = "postgresql+asyncpg://admin:admin@postgres_container:5432/admin"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 # expire_on_commit=False will prevent attributes from being expired
--- P_A_WORK/BACK/schemas.py
+++ P_A_WORK/BACK/schemas.py
@@ -3,30 +3,31 @@ from pydantic import BaseModel, ConfigDict, Field
 from typing import List, Optional
 
 
-
-
 class ResultOut(BaseModel):
     result: bool
-    
+
+
 class ErrResultOut(ResultOut):
     error_type: str
     error_message: str
-    
-    
-    
+
+
 class TweetResultOut(ResultOut):
     tweet_id: int
 
+
 class MediaResultOut(ResultOut):
     media_id: int
-   
+
 
 # USERS
+
 
 class ShortUser(BaseModel):
     id: int
     name: str
-    
+
+
 class UserModel(ShortUser):
     followers: List[ShortUser]
     following: List[ShortUser]
@@ -36,35 +37,34 @@ class UserModel(ShortUser):
 
 class UserResultOut(ResultOut):
     user: UserModel
-    
+
 
 # TWEETS
 
+
 class TweetIn(BaseModel):
-    """ 
-    """
+    """ """
+
     tweet_data: str
     tweet_media_ids: Optional[List[int]] = None
 
-class PictureOut(BaseModel):
-    ...
 
-class TweetOut(BaseModel): #TweetIn):
-    """
-    """
+class PictureOut(BaseModel): ...
+
+
+class TweetOut(BaseModel):  # TweetIn):
+    """ """
+
     content: str
     # не очень понял, что за список, каким образом он передается фронтУ.
     attachments: Optional[List[str]] = None
-    
+
     id: int
     author: ShortUser
     likes: Optional[List[ShortUser]] = None
-    
-    
+
     model_config = ConfigDict(from_attributes=True)
-    
-    
+
+
 class TweetResultListOut(ResultOut):
     tweets: List[TweetOut] = None
-
-
